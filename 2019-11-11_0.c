02ccd5a8d572a8390c6803ee35c77bd30b43b566
yanorei32 <yanorei@hotmail.co.jp>
2019-11-11 17:25:41 +0900

    trash optimized for c

--- 2019-11-11_0.c
+++ 2019-11-11_0.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int, char**) {
+main() {
 	int i;
 
-	for (i = 1;i <= 16384;i += i) {
+	for (i = 1;i <= 16384;i += i)
 		printf(
 			" printf() returned %d.\n",
 			printf("%d", i)
 		);
-	}
-
-	return EXIT_SUCCESS;
 }
 

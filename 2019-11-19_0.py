e37a2ec564c3137be07ca6ec64c93121fda8d312
Yanorei32 <yanorei@hotmail.co.jp>
2019-11-19 23:17:16 +0900

    Add trash

---
+++ 2019-11-19_0.py
@@ -0,0 +1,36 @@
+import random
+import sys
+import time
+
+def main():
+    targetStr = list("ポプテピピック")
+    targetStrLen = len(targetStr)
+    matchCounter = 0
+    tryCounter = 0
+
+    begin = time.time()
+
+    while True:
+        tryCounter += 1
+
+        c = random.choice(targetStr)
+
+        sys.stdout.write(c)
+
+        if c != targetStr[matchCounter]:
+            matchCounter = 0
+            continue
+
+        if matchCounter == targetStrLen - 1:
+            break
+
+        matchCounter += 1
+
+    deltaTime = time.time() - begin
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+    print(f'Done ({tryCounter} chars printed. Δt={deltaTime} sec)')
+
+if __name__ == '__main__':
+    main()
+

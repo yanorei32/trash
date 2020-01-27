f8aca6997b9b7ebca5c23cec21704efa86bb81fd
Yanorei32 <yanorei@hotmail.co.jp>
2020-01-27 15:31:10 +0900

    update trash

--- 2020-01-27_2.c
+++ 2020-01-27_2.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <assert.h>
+
 
 char itoc(unsigned int n);
 
 inline char itoc(unsigned int n) {
+	assert(n - 10 > 'Z' && "itoc: Out of range 0-9A-Z");
 	return n < 10 ? n + '0' : n - 10 + 'A';
 }
 
-void itob(unsigned int n, char s[], int b) {
+void itob(unsigned int n, int b, char s[], int buflen) {
 	unsigned int charLen, n_copy;
 
 	charLen = 0;
@@ -14,7 +17,8 @@ void itob(unsigned int n, char s[], int b) {
 	n_copy = n;
 
 	while ((n_copy /= b) >= 1) {
-		charLen++;
+		++charLen;
+		assert(charLen+1 < buflen && "itob: buflen is too short");
 	}
 
 	s[++charLen] = 0;
@@ -26,15 +30,15 @@ void itob(unsigned int n, char s[], int b) {
 }
 
 int main() {
-	char a[128];
+	char a[6];
 
-	itob(32, a, 2);
+	itob(32, 10, a, sizeof(a)/sizeof(char));
 	printf("%s\n", a);
 
-	itob(32, a, 10);
+	itob(32, 16, a, sizeof(a)/sizeof(char));
 	printf("%s\n", a);
 
-	itob(32, a, 16);
+	itob(32, 2, a, sizeof(a)/sizeof(char));
 	printf("%s\n", a);
 }
 

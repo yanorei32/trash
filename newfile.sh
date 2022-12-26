f101eef61bf6ed6c8a74d8175f130b539a3cf175
yanorei32 <yanorei@hotmail.co.jp>
2022-12-26 16:10:35 +0900

    Update newfile.sh

--- newfile.sh
+++ newfile.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-read -p "File Extension: " FILE_EXT
+if [[ $# == 1 ]]; then
+	FILE_EXT="$1"
+else
+	read -r -p "File Extension: " FILE_EXT
+fi
 
 NUM=0
-DATE=`date '+%Y-%m-%d'`
+DATE="$(date '+%Y-%m-%d')"
 
 while : ; do
-	FILENAME=${DATE}_${NUM}.${FILE_EXT}
+	f=${DATE}_${NUM}.${FILE_EXT}
 
-	if [ ! -e $FILENAME ]; then
+	if [ ! -e "$f" ]; then
 		break
 	fi
 
 	NUM=$(( NUM + 1 ))
 done
 
-echo New file openned: $FILENAME
+export f
 
-vim $FILENAME
+# backward-compat
+export FILENAME="$f"
 
+echo "New file openned: $f"
+
+vim "$f"
